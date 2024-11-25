0e4840e889e1d2fa870d90b4586b1c37ab49144a
sallimar <sallimarxpplg1@gmail.com>
2024-11-25 13:15:32 +0700

    ke3

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,8 @@ import 'package:perpustakaan/home_page.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'home_page.dart';
 
-Future<void> main() async {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Supabase.initialize(
     url: 'https://lxgtjjqmfioktfqlesrq.supabase.co', 
     anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx4Z3RqanFtZmlva3RmcWxlc3JxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzE1NTM4MzcsImV4cCI6MjA0NzEyOTgzN30.UDqMPGi3obf-tCD89HjmmR5PwaRkbmC0EYfhMjylEBQ');
